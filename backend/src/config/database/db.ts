337c6a814e1a7b955e3441cb9dfba00f01f88fc4
EthanVo <vvotandat78@gmail.com>
2026-07-15 17:27:13 +0700

    change db uri

--- backend/src/config/database/db.ts
+++ backend/src/config/database/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 function connectDB() {
-  const DB_URI = 'mongodb://127.0.0.1:27017/ecommerce';
+  const DB_URI = process.env.DB_URI as string;
   mongoose.connect(DB_URI);
 }
 
